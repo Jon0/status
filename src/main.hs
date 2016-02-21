ce8db8a6918ea6c50e0d719c3d86ce85b90de779
Jon0 <Jono4728@gmail.com>
2016-02-21 17:36:50 +1300

    main loop

--- src/main.hs
+++ src/main.hs
@@ -3,5 +3,32 @@ import System.IO
 
 main :: IO ()
 main = do
+    s <- get_string
+    putStr s
     hGetChar stdin
-    print "hello\n"
+    putStr "hello\n"
+
+get_string :: IO String
+get_string = do
+    c <- getChar
+    if c == '\n'
+    then return ""
+    else do
+        l <- get_string
+        return (c:l)
+
+finished :: Int -> Bool
+finished i = False
+
+inc_state :: Int -> Int
+inc_state i = i
+
+main_loop :: [Int] -> [Int]
+main_loop loop_state = do {
+    some_state <- loop_state;
+    --new_loop_state <- inc_state loop_state;
+    --if finished new_loop_state
+    --then return new_loop_state
+    --else main_loop new_loop_state
+    return some_state;
+}
