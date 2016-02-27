f24f54e09109c6201eac89e4cc2fe59fbf373748
Jon0 <Jono4728@gmail.com>
2016-02-27 16:35:55 +1300

    types

--- src/Route.hs
+++ src/Route.hs
@@ -1,5 +1,6 @@
 module Route where
 
+import Network.Socket
 import Network.Stream
 import Network.HTTP
 
@@ -14,13 +15,24 @@ test_fn = Sometype 0
 req_handler :: Request String -> (String, Bool)
 req_handler request = ("test", True)
 
+req_handler2 :: Request a -> (String, Bool)
+req_handler2 request = ("test", True)
+
 read_request :: (Stream s) => s -> IO String
-read_request str = do
+read_request s = do return "test"
+    --do
     --input <- receiveHTTP str
     --case input of
     --    Nothing -> do
     --        return "error"
     --    Just request -> do
+    --        putStrLn $ receiveHTTP str
     --        (response, done) <- req_handler request
     --        return response
-    return "."
+    --return "."
+read_request _ = do return "..."
+
+read_requests :: HandleStream Socket -> IO String
+read_requests sock = do
+    -- http_data <- receiveHTTP sock
+    return "test"
