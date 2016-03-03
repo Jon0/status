5aedcf267285925f7af23fdc51996eb00128ddcc
Jon0 <Jono4728@gmail.com>
2016-03-03 15:44:26 +1300

    tmpl

--- src/Template.hs
+++ src/Template.hs
@@ -1,4 +1,10 @@
 module Template where
 
+
+data Template = Static String | Dynamic String
+
+temlate_file :: FilePath -> Template
+template_file path = Static "data"
+
 tmpl :: String -> String
 tmpl s = s
