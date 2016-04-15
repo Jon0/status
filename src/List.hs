693c7ea32beab68771c445e9e1d88f6dd6620494
Jon0 <Jono4728@gmail.com>
2016-04-15 17:24:17 +1200

    sorted lists

--- src/List.hs
+++ src/List.hs
@@ -1,5 +1,6 @@
 module List where
 
+import Data.List
 import Document
 import Html
 
@@ -7,12 +8,19 @@ import Html
 renderListPage :: (Renderable r) => String -> [r] -> HtmlContent
 renderListPage title rd = (renderList rd)
 
+
 renderList :: (Renderable r) => [r] -> HtmlContent
 renderList [] = labelHtml ""
 renderList rd = HtmlContent $ HtmlTable (map renderAll rd)
 
+
 renderListDiv :: (Renderable r) => [r] -> HtmlContent
 renderListDiv rd = createDiv "list" $ [(renderList rd)]
 
+
 renderZipDiv :: (Renderable (a, r)) => a -> [r] -> HtmlContent
 renderZipDiv ad rd = renderListDiv (zip (cycle [ad]) rd)
+
+
+renderSortedList :: (Renderable r) => (r -> r -> Ordering) -> [r] -> HtmlContent
+renderSortedList order items = renderListDiv (sortBy order items)
